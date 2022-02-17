7f4c24412298b6ee7f5fed55c80681758691f822
emilyhmarkowitz <emilymarkowitz@gmail.com>
2022-02-17 13:14:46 -0800

    fixed file.copy() to work

--- code/run.R
+++ code/run.R
@@ -21,19 +21,27 @@ source('./code/data.R')
 # Check work -------------------------------------------------------------------
 
 for (i in 1:nrow(data1)) {
+  
   dat <- data1[i,]
+  
   file_name <- paste0(maxyr, "-", dat$last_name, "-", 
                       janitor::make_clean_names(dat$title), ".docx")
+  
   rmarkdown::render(paste0("./code/template.Rmd"),
                     output_dir = dir_out,
                     output_file = paste0(dir_out, file_name))
-  temp=strsplit(x = dat$survey, split = ", ")[[1]]
-for (ii in 1:length(temp)) {
-  dir.create(paste0(dir_out, temp[ii]), showWarnings = F)
-  file.copy(file_name, from=dir_out, to=paste0(dir_out, temp[ii]), overwrite = T)
-
+  
+  temp <- strsplit(x = dat$survey, split = ", ")[[1]]
+  
+  for (ii in 1:length(temp)) {
+    
+    dir.create(paste0(dir_out, temp[ii]), showWarnings = F)
+    
+    file.copy(from = paste0(dir_out, file_name), 
+              to = paste0(dir_out, temp[ii], "/", file_name), 
+              overwrite = TRUE)
+    
   }
-
 
 }
 
